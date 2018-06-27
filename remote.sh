9414508171823eef3c1cf6d82e3a127dee8a2f40
ivanp7 <ivanp7.git@gmail.com>
2018-06-27 18:17:50 +0300

    Rename 'check' command to 'status' in remote.sh

--- remote.sh
+++ remote.sh
@@ -4,20 +4,17 @@ USERNAME=ivanp7
 LOCAL_PORT=65535
 
 # Known servers
-if [[ "$2" == "roboticslab" ]]
-then
+if [[ "$2" == "roboticslab" ]]; then
     HOSTNAME=roboticslab
     PORT=62222
     WAKEUP_PORT=40000
     MAC_ADDRESS="38:60:77:72:d5:a0"
-elif [[ "$2" == "home-black" ]]
-then
+elif [[ "$2" == "home-black" ]]; then
     HOSTNAME=home
     PORT=62222
     WAKEUP_PORT=61111
     MAC_ADDRESS="40:61:86:63:db:cc"
-elif [[ "$2" == "home-white" ]]
-then
+elif [[ "$2" == "home-white" ]]; then
     HOSTNAME=home
     PORT=62223
     WAKEUP_PORT=61112
@@ -27,26 +24,20 @@ else
     exit 2
 fi
 
-if [[ "$1" == "check" ]]
-then
+if [[ "$1" == "status" ]]; then
     COMMAND="nc -z $HOSTNAME $PORT"
-elif [[ "$1" == "wakeup" ]]
-then
+elif [[ "$1" == "wakeup" ]]; then
     COMMAND="wol -p $WAKEUP_PORT -i $HOSTNAME $MAC_ADDRESS"
 
-elif [[ "$1" == "upload" ]]
-then
+elif [[ "$1" == "upload" ]]; then
     COMMAND="rsync -avrP -e 'ssh -p $PORT' \"$3\" $USERNAME@$HOSTNAME:\"$4\""
-elif [[ "$1" == "download" ]]
-then
+elif [[ "$1" == "download" ]]; then
     COMMAND="rsync -avrP -e 'ssh -p $PORT' $USERNAME@$HOSTNAME:\"$3\" \"$4\""
 
-elif [[ "$1" == "command" ]]
-then
+elif [[ "$1" == "command" ]]; then
     SSH_FLAGS="-p $PORT"
     COMMAND="TERM=xterm-256color ssh $SSH_FLAGS $USERNAME@$HOSTNAME ${@:3}"
-elif [[ "$1" == "tunnel" ]]
-then
+elif [[ "$1" == "tunnel" ]]; then
     SSH_FLAGS="-D $LOCAL_PORT -N -p $PORT"
     COMMAND="TERM=xterm-256color ssh $SSH_FLAGS $USERNAME@$HOSTNAME"
 
