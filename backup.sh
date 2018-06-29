8a3131416ce851a45e4e504ed7386bc493a72ef6
ivanp7 <ivanp7.git@gmail.com>
2018-06-29 15:26:24 +0300

    Fix incorrect backup destination

--- backup.sh
+++ backup.sh
@@ -2,8 +2,8 @@
 
 if [[ "$1" == "keys" ]]; then
     rsync -avP --force --delete ~/Personal/Keys/ ~/Dropbox/Keys/
-elif [[ "$1" == "data-to-disk" ]]; then
-    rsync -avP --force --delete ~/HostShared/Workspace/Data/ /mnt/usb-drive/Backup/Workspace/Data/
+elif [[ "$1" == "data-to-drive" ]]; then
+    rsync -avP --force --delete ~/HostShared/Workspace/Data/ /mnt/mount-$2/Backup/Workspace/Data/
 elif [[ "$1" == "data-to-cloud" ]]; then
     rsync -avP --force --delete -e 'ssh -p 62222' ~/HostShared/Workspace/Data/ ivanp7@home:/mnt/backup/ivanp7/Workspace/Data/
 else
