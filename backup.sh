de3ed57fe988c5c4e76c300f7ecdecd4f0ffe54d
ivanp7 <ivanp7.git@gmail.com>
2018-08-26 04:29:23 +0300

    Add "vm" option for backing up virtual machines in backup.sh

--- backup.sh
+++ backup.sh
@@ -2,6 +2,12 @@
 
 if [[ "$1" == "keys" ]]; then
     rsync -avP --force --delete ~/Personal/Keys/ ~/Dropbox/Keys/
+elif [[ "$1" == "vm" ]]; then
+    if [[ -z "$2" ]]; then
+        remote.sh upload home-primary ~/HostShared/VirtualMachines/ /mnt/backup/ivanp7/VirtualMachines/
+    else
+        rsync -avP ~/HostShared/VirtualMachines/ $2
+    fi
 elif [[ "$1" == "data" ]]; then
     if [[ -z "$2" ]]; then
         remote.sh upload home-primary ~/HostShared/Workspace/Data/ /mnt/backup/ivanp7/Workspace/Data/ --force --delete
