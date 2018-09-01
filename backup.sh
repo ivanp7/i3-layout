39e13637463d1f2b95ea928324d806fba6bdad69
ivanp7 <ivanp7.git@gmail.com>
2018-09-02 00:53:54 +0300

    Update home-primary backup directory paths

--- backup.sh
+++ backup.sh
@@ -4,13 +4,13 @@ if [[ "$1" == "keys" ]]; then
     rsync -avP --force --delete ~/Personal/Keys/ ~/Dropbox/Keys/
 elif [[ "$1" == "vm" ]]; then
     if [[ -z "$2" ]]; then
-        remote.sh upload home-primary ~/HostShared/VirtualMachines/ /mnt/backup/ivanp7/VirtualMachines/
+        remote.sh upload home-primary ~/HostShared/VirtualMachines/ /mnt/backup/Disk/VirtualMachines/
     else
         rsync -avP ~/HostShared/VirtualMachines/ $2
     fi
 elif [[ "$1" == "data" ]]; then
     if [[ -z "$2" ]]; then
-        remote.sh upload home-primary ~/HostShared/Workspace/Data/ /mnt/backup/ivanp7/Workspace/Data/ --force --delete
+        remote.sh upload home-primary ~/HostShared/Workspace/Data/ /mnt/backup/Disk/Workspace/Data/ --force --delete
     else
         rsync -avP --force --delete ~/HostShared/Workspace/Data/ $2
     fi
