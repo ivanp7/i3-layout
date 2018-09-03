d85d511f7eef5bd7480f9da4f0911127f169727b
ivanp7 <ivanp7.git@gmail.com>
2018-09-03 08:46:13 +0300

    Improve virtual machines backup method

--- backup.sh
+++ backup.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+CLOUD_HOSTNAME=home-primary
+
 if [[ "$1" == "keys" ]]; then
-    rsync -avP --force --delete ~/Personal/Keys/ ~/Dropbox/Keys/
+    rsync -avP --force --delete "~/Personal/Keys/" "~/Dropbox/Keys/"
+
 elif [[ "$1" == "vm" ]]; then
     if [[ -z "$2" ]]; then
-        remote.sh upload home-primary ~/HostShared/VirtualMachines/ /mnt/backup/Disk/VirtualMachines/Used/
+        remote.sh upload $CLOUD_HOSTNAME "~/HostShared/VirtualMachines/" "/mnt/backup/Disk/VirtualMachines/Used/"
     else
-        rsync -avP ~/HostShared/VirtualMachines/ $2
+        if [[ -z "$3" ]]; then
+            remote.sh upload $CLOUD_HOSTNAME "~/VirtualMachines/$2" "/mnt/backup/Disk/VirtualMachines/Used/"
+        else
+            rsync -avP "~/VirtualMachines/$2" "$3"
+        fi
     fi
+
 elif [[ "$1" == "data" ]]; then
     if [[ -z "$2" ]]; then
-        remote.sh upload home-primary ~/HostShared/Workspace/Data/ /mnt/backup/Disk/Workspace/Data/ --force --delete
+        remote.sh upload $CLOUD_HOSTNAME "~/HostShared/Workspace/Data/" "/mnt/backup/Disk/Workspace/Data/" --force --delete
     else
-        rsync -avP --force --delete ~/HostShared/Workspace/Data/ $2
+        rsync -avP --force --delete "~/HostShared/Workspace/Data/" "$2"
     fi
+
 else
     echo Error: unknown command "$1"
     exit 1
