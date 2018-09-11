c19b3cd519f07a5d33d5911e99a04089d8d07eeb
ivanp7 <ivanp7.git@gmail.com>
2018-09-11 16:24:03 +0300

    Replace ~ with $HOME in paths

--- backup.sh
+++ backup.sh
@@ -3,24 +3,24 @@
 CLOUD_HOSTNAME=home-primary
 
 if [[ "$1" == "keys" ]]; then
-    rsync -avP --force --delete "~/Personal/Keys/" "~/Dropbox/Keys/"
+    rsync -avP --force --delete "$HOME/Personal/Keys/" "$HOME/Dropbox/Keys/"
 
 elif [[ "$1" == "vm" ]]; then
     if [[ -z "$2" ]]; then
-        remote.sh upload $CLOUD_HOSTNAME "~/HostShared/VirtualMachines/" "/mnt/backup/Disk/VirtualMachines/Used/"
+        remote.sh upload $CLOUD_HOSTNAME "$HOME/HostShared/VirtualMachines/" "/mnt/backup/Disk/VirtualMachines/Used/"
     else
         if [[ -z "$3" ]]; then
-            remote.sh upload $CLOUD_HOSTNAME "~/VirtualMachines/$2" "/mnt/backup/Disk/VirtualMachines/Used/"
+            remote.sh upload $CLOUD_HOSTNAME "$HOME/VirtualMachines/$2" "/mnt/backup/Disk/VirtualMachines/Used/"
         else
-            rsync -avP "~/VirtualMachines/$2" "$3"
+            rsync -avP "$HOME/VirtualMachines/$2" "$3"
         fi
     fi
 
 elif [[ "$1" == "data" ]]; then
     if [[ -z "$2" ]]; then
-        remote.sh upload $CLOUD_HOSTNAME "~/HostShared/Workspace/Data/" "/mnt/backup/Disk/Workspace/Data/" --force --delete
+        remote.sh upload $CLOUD_HOSTNAME "$HOME/HostShared/Workspace/Data/" "/mnt/backup/Disk/Workspace/Data/" --force --delete
     else
-        rsync -avP --force --delete "~/HostShared/Workspace/Data/" "$2"
+        rsync -avP --force --delete "$HOME/HostShared/Workspace/Data/" "$2"
     fi
 
 else
