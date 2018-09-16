e0cc7268bb713c534c7bc95795644e2f2825dd9b
ivanp7 <ivanp7.git@gmail.com>
2018-09-17 01:43:47 +0300

    Update rsync flags in the backup command to the home cloud

--- backup.sh
+++ backup.sh
@@ -7,14 +7,10 @@ if [[ "$1" == "keys" ]]; then
     rsync -avP --force --delete "$HOME/Personal/Keys/" "$HOME/Dropbox/Keys/"
 
 elif [[ "$1" == "vm" ]]; then
-    remote.sh upload $CLOUD_HOSTNAME "$HOME/HostShared/VirtualMachines/" "$CLOUD_BACKUP_DIRECTORY/VirtualMachines/Used/"
+    remote.sh $CLOUD_HOSTNAME upload "$HOME/HostShared/VirtualMachines/" "$CLOUD_BACKUP_DIRECTORY/VirtualMachines/Used/" -a --groupmap=*:server
 
 elif [[ "$1" == "data" ]]; then
-    if [[ -z "$2" ]]; then
-        remote.sh upload $CLOUD_HOSTNAME "$HOME/HostShared/Workspace/Data/" "$CLOUD_BACKUP_DIRECTORY/Workspace/Data/" --force --delete
-    else
-        rsync -avP --force --delete "$HOME/HostShared/Workspace/Data/" "$2"
-    fi
+    remote.sh $CLOUD_HOSTNAME upload "$HOME/HostShared/Workspace/Data/" "$CLOUD_BACKUP_DIRECTORY/Workspace/Data/" -a --groupmap=*:server --force --delete
 
 else
     echo Error: unknown command "$1"
