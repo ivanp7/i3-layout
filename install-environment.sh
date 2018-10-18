94571b427ee4b6c1ab1c5a71d1626391317bce66
ivanp7 <ivanp7.git@gmail.com>
2018-10-18 15:15:54 +0300

    Add instructions for setting resolution to 1920x1080

--- install-environment.sh
+++ install-environment.sh
@@ -42,6 +42,31 @@ chmod 644 .ssh/id_rsa.pub
 
 ####################################################################
 
+echo "Setting default graphics mode: 1920x1080, 60 Hz, 24 bits depth..."
+echo $'
+Section "Monitor"
+Identifier "Virtual1"
+ModeLine "1920x1080_60.00" 173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+Option "PreferredMode" "1920x1080_60.00"
+EndSection
+
+Section "Screen"
+Identifier "Screen0"
+Monitor "Virtual1"
+DefaultDepth 24
+SubSection "Display"
+Modes "1920x1080_60.00"
+EndSubSection
+EndSection
+
+Section "Device"
+Identifier "Device0"
+Driver "vmware"
+EndSection
+' | sudo tee /etc/X11/xorg.conf.d/90-resolution.conf
+
+####################################################################
+
 echo "Installing keepassxc, telegram-desktop, qbittorrent, dropbox..."
 yay -S keepassxc telegram-desktop qbittorrent dropbox
 
