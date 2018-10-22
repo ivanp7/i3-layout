ea82ed8e18469705969d0f448b6f860a6355d415
ivanp7 <ivanp7.git@gmail.com>
2018-10-22 16:38:10 +0300

    Move workspace 1 layout to x-dotfiles

--- layout/load-layout.sh
+++ layout/load-layout.sh
@@ -1,43 +1,11 @@
 #!/bin/bash
 
 # Building this layout
-# workspace 1:
-# ____________________________________
-# |                     |             |
-# |                     |             |
-# |                     |  tty-clock  |
-# |                     |             |
-# |                     |_____________|
-# |                     |             |
-# |       termite       |   when ci   |
-# |                     |             |
-# |                     |-------------|
-# |                     |             |
-# |                     |   todo ls   |
-# |                     |             |
-# |_____________________|_____________|
 #
 # workspace 8: firefox
 # workspace 9: keepassxc
 # workspace 10: telegram-desktop
 #
-# It may be usefull to disable mouse pointer and/or touchpad while
-# layouting.
-#
-# Disable mouse:
-# mouseID=`xinput list | grep -Eo 'Mouse\s.*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
-# xinput set-prop $mouseID "Device Enabled" 0
-#
-# Enable mouse:
-# xinput set-prop $mouseID "Device Enabled" 1
-#
-# Disable the touchpad:
-# touchID=`xinput list | grep -Eo 'TouchPad\s*id\=[0-9]{1,2}' | grep -Eo '[0-9]{1,2}'`
-# xinput set-prop $touchID "Device Enabled" 0
-#
-# Enable the touchpad
-# xinput set-prop $touchID "Device Enabled" 1
-
 # LAYOUT
 # ==========
 
@@ -45,8 +13,7 @@ start_app()
 {
     if ! pgrep ${1:0:15}; then
         i3-msg "exec $1"
-        sleep 1
-        sh $HOME/.config/i3/wait-and-move.sh `pidof -s $1` 1 $2
+        sh $HOME/.config/i3/bin/wait-and-move.sh $1 1 $2
     fi
 }
 
@@ -61,17 +28,4 @@ start_app telegram-desktop 10
 # termite --exec "sh $HOME/scripts/kpcli.sh" --title "KeePass" &
 
 # sleep 1
-
-i3-msg workspace "1"
-
-i3-msg "append_layout $HOME/scripts/layout/workspace_1.json"
-termite --exec "tty-clock -c -b -n -C 6" --title "Clock" &
-termite --exec "sh $HOME/scripts/layout/update_calendar.sh" --title "Calendar" &
-termite --exec "sh $HOME/scripts/layout/update_todo.sh" --title "To-Do List" &
-termite --exec "bash -c 'echo; neofetch; bash'" --title "Terminal1" &
-
-sleep 1.0
-i3-msg '[con_mark="primary_terminal"] focus'
-sleep 3.0
-i3-msg '[con_mark="primary_terminal"] focus'
 
