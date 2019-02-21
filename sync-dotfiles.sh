33960632d5b4c7f71cf039d7dc021b3d129eebdd
ivanp7 <ivanp7.git@gmail.com>
2019-02-21 15:56:01 +0300

    Improve sync-dotfiles.sh script

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -2,9 +2,13 @@
 
 SYNC_COMMAND='"cd /home/shared/dotfiles; git pull; cd /home/shared/x-dotfiles; git pull"'
 
-remote.sh roboticslab command "$SYNC_COMMAND"
-remote.sh home-primary wakeup
-remote.sh home-primary command "$SYNC_COMMAND"
-remote.sh home-secondary wakeup
-remote.sh home-secondary command "$SYNC_COMMAND"
+function sync_dotfiles ()
+{
+    remote.sh $1 wakeup
+    remote.sh $1 command "$SYNC_COMMAND"
+}
+
+sync_dotfiles roboticslab
+sync_dotfiles home-primary
+sync_dotfiles home-secondary
 
