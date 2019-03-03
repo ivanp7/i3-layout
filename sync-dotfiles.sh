7f39a3b21fdf8debd91b441e91f1e8ee9fcaabcf
ivanp7 <ivanp7.git@gmail.com>
2019-03-04 01:24:07 +0300

    Add sleep before connecting to remote hosts in sync-dotfiles.sh

--- sync-dotfiles.sh
+++ sync-dotfiles.sh
@@ -5,10 +5,11 @@ SYNC_COMMAND='"cd /home/shared/dotfiles; git pull; cd /home/shared/x-dotfiles; g
 function sync_dotfiles ()
 {
     remote.sh $1 wakeup
+    sleep $2
     remote.sh $1 command "$SYNC_COMMAND"
 }
 
-sync_dotfiles roboticslab
-sync_dotfiles home-primary
-sync_dotfiles home-secondary
+sync_dotfiles roboticslab 0
+sync_dotfiles home-primary 10
+sync_dotfiles home-secondary 60
 
