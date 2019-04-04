06fbb89916705b896ce98474ea78d1eacbd77b9e
ivanp7 <ivanp7.git@gmail.com>
2019-04-04 03:42:38 +0300

    Start termite with title

--- layout/load-layout.sh
+++ layout/load-layout.sh
@@ -21,6 +21,6 @@ start_app firefox 9
 i3-msg "rename workspace 9 to 9: Firefox"
 start_app telegram-desktop 10
 i3-msg "rename workspace 10 to 10: Telegram"
-start_app termite 1
+start_app "termite --title 'Terminal'" 1
 i3-msg "rename workspace 1 to 1: Terminal"
 
